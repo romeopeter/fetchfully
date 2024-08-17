85cb9b441732d88eb280907bb1d999f98f2e9fb3
romeo <romeomeshack34@gmail.com>
2024-08-17 15:59:30 +0100

    Types and interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,21 @@
+export type CustomOptionsType = {
+  responseBodyType:
+    | "text"
+    | "json"
+    | "formData"
+    | "blob"
+    | "arrayBuffer"
+    | "body";
+  timeout: number;
+};
+
+export type FetchAPIPropsType = {
+  URL: string;
+  method?: "GET" | "POST" | "PATCH" | "DELETE" | "HEAD" | "OPTIONS";
+  headers?: { [name: string]: string };
+  body?: string;
+  credentials?: "same-origin" | "omit" | "include";
+  keepalive?: boolean;
+  mode?: "cors" | "same-origin" | "no-cors";
+  customOptions: CustomOptionsType;
+};
