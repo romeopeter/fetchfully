f45f2597fa242639e8b51c3ffa8ce862d21d0e3e
romeo <romeomeshack34@gmail.com>
2024-09-30 13:19:32 +0100

    Modified

--- src/requestQuery/index.ts
+++ src/requestQuery/index.ts
@@ -1,53 +1,32 @@
+import dataByContentType from "../data-by-content-type";
+
+/* ------------------------------------------------------ */
+
+type DataByContentTypeProps = {
+  data: Promise<any>;
+  responseObject: Response;
+};
+
 /**
- * Handles all GET request cases
+ * Handles all non-mutation request (GET) cases.
  *
  * @param response Response: Fetch API response interface to a request.
  *
  * @returns Promise<any>
  */
-export default function requestQuery(response: Response) {
-  const contentType = response.headers.get("Content-Type") as string;
+export default function requestQuery(
+  response: Response
+): DataByContentTypeProps {
 
   if (!response.ok) {
     const errorData = {
-      error: `HTTP error! status: ${response.status}`,
+      error: "REQUEST ERROR",
+      status: response.status,
       responseObject: response,
     };
 
     throw new Error(JSON.stringify(errorData));
   }
 
-  if (contentType.includes("application/json")) {
-    return {
-      data: response.json(),
-      responseObject: response,
-    };
-  }
-
-  if (contentType.includes("application/octet-stream")) {
-    return {
-      data: response.arrayBuffer(),
-      responseObject: response,
-    };
-  }
-
-  if (contentType.includes("multipart/form-data")) {
-    return {
-      data: response.formData(),
-      responseObject: response,
-    };
-  }
-
-  if (contentType.includes("image/")) {
-    return {
-      data: response.blob(),
-      responseObject: response,
-    };
-  }
-
-  // Default response to text if content type is unknown
-  return {
-    data: response.text(),
-    responseObject: response,
-  };
+  return dataByContentType(response);
 }
