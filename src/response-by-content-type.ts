a71b7622537dcb032f702161b24e78e1dd1488cf
romeo <romeomeshack34@gmail.com>
2024-11-14 10:37:13 +0100

    Response data by content type

---
+++ src/response-by-content-type.ts
@@ -0,0 +1,47 @@
+import { ResponseByContentTypeProps } from "./types";
+
+/* ------------------------------------------------------*/ 
+
+/**
+ * Returns formatted data according to response content type.
+ * 
+ * @param response: Response
+ * @returns ResponseByContentTypeProps
+ */
+export default function ResponseByContentType(response: Response): ResponseByContentTypeProps {
+  const contentType = response.headers.get("Content-Type") as string;
+
+  if (contentType.includes("application/json")) {
+    return {
+      data: response.json(),
+      responseObject: response,
+    };
+  }
+
+  if (contentType.includes("application/octet-stream")) {
+    return {
+      data: response.arrayBuffer(),
+      responseObject: response,
+    };
+  }
+
+  if (contentType.includes("multipart/form-data")) {
+    return {
+      data: response.formData(),
+      responseObject: response,
+    };
+  }
+
+  if (contentType.includes("image/")) {
+    return {
+      data: response.blob(),
+      responseObject: response,
+    };
+  }
+
+  // Default response as text if content type is unknown
+  return {
+    data: response.text(),
+    responseObject: response,
+  };
+}
