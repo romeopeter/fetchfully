b5e68da081b664b14426a27cd7142e2a59273592
romeo <romeomeshack34@gmail.com>
2024-11-18 00:45:41 +0100

    renamed returned response object with an _

--- src/response-by-content-type.ts
+++ src/response-by-content-type.ts
@@ -1,47 +1,57 @@
 import { ResponseByContentTypeProps } from "./types";
 
-/* ------------------------------------------------------*/ 
+/* ------------------------------------------------------*/
 
 /**
  * Returns formatted data according to response content type.
- * 
+ *
  * @param response: Response
- * @returns ResponseByContentTypeProps
+ * @returns Promise<ResponseByContentTypeProps>
  */
-export default function responseByContentType(response: Response): ResponseByContentTypeProps {
+export default async function responseByContentType(
+  response: Response
+): Promise<ResponseByContentTypeProps> {
   const contentType = response.headers.get("Content-Type") as string;
 
   if (contentType.includes("application/json")) {
+    const data = await response.json();
+
     return {
-      data: response.json(),
-      responseObject: response,
+      data: data,
+      _responseObject: response,
     };
   }
 
   if (contentType.includes("application/octet-stream")) {
+    const data = await response.arrayBuffer();
+
     return {
-      data: response.arrayBuffer(),
-      responseObject: response,
+      data: data,
+      _responseObject: response,
     };
   }
 
   if (contentType.includes("multipart/form-data")) {
+    const data = await response.formData();
+
     return {
-      data: response.formData(),
-      responseObject: response,
+      data: data,
+      _responseObject: response,
     };
   }
 
   if (contentType.includes("image/")) {
+    const data = await response.blob();
+
     return {
-      data: response.blob(),
-      responseObject: response,
+      data: data,
+      _responseObject: response,
     };
   }
 
   // Default response as text if content type is unknown
   return {
-    data: response.text(),
-    responseObject: response,
+    data: await response.text(),
+    _responseObject: response,
   };
 }
