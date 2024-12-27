8edb19ad9ee32b9fdc01744051c0c9dd5b5c7070
romeo <romeomeshack34@gmail.com>
2024-12-27 23:41:13 +0100

    types

---
+++ src/types/config.ts
@@ -0,0 +1,24 @@
+// Fetchfully core configuration
+export type FetchfullyConfig = {
+  baseUrl?: string;
+  url?: string;
+  path?: string | string[];
+  query?: Record<string, any>;
+  method?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+  body?: any;
+  headers?: { [name: string]: any };
+  credentials?: "same-origin" | "omit" | "include";
+  keepalive?: boolean;
+  mode?: "same-origin" | "cors" | "no-cors";
+  customOptions?: {
+    timeout?: number;
+    queryArrayFormat: "brackets" | "comma" | "repeat" | "none";
+  };
+};
+
+// Fetchfully instance structure
+export type FetchfullyInstance = {
+  (config: FetchfullyConfig): Promise<any>;
+  defaults: FetchfullyConfig;
+  create: (config?: FetchfullyConfig) => FetchfullyInstance;
+};
---
+++ src/types/request-response-by-content.ts
@@ -0,0 +1,5 @@
+// Request's response data structure returned
+export type ResponseByContentTypeProps = {
+    data: any;
+    _responseObject: Response;
+  };
