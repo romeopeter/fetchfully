b5c4851069fe2f325ca08a44d9d7fb5282da6f9b
romeo <romeomeshack34@gmail.com>
2025-01-06 02:58:14 +0100

    Main export

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { mergeConfig } from "./utils/mergeConfig";
 const defaultFetchfullyInstance = createFetcher(defaults) as FetchfullyInstance;
 
 // Factory method to produce a new fetcher instance
-defaultFetchfullyInstance.createFetcher = (config?: FetchfullyConfig) => {
+defaultFetchfullyInstance.create = (config?: FetchfullyConfig) => {
   const instanceConfig = mergeConfig(defaults, config || {});
 
   return createFetcher(instanceConfig) as FetchfullyInstance;
