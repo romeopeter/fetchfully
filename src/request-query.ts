8bae6b0ae563e10d9e2ca03d1e7e00546f14b305
romeo <romeomeshack34@gmail.com>
2025-06-15 23:01:44 +0100

    Modified request query.

--- src/request-query.ts
+++ src/request-query.ts
@@ -1,5 +1,4 @@
 import responseByContentType from "./utils/response-by-content-type";
-import { ResponseByContentType } from "./types/request-response-by-content";
 import { HttpError } from "./utils/custom-request-errors";
 
 /* ------------------------------------------------------ */
@@ -7,13 +6,11 @@ import { HttpError } from "./utils/custom-request-errors";
 /**
  * Handles all non-mutation request (GET) cases.
  *
- * @param response Response: Fetch API response interface to a request.
- *
+ * @param response Response: Fetch API response interface for a request.
+ * 
  * @returns Promise<any>
  */
-export default function requestQuery(
-  response: Response
-): Promise<ResponseByContentType> {
+export default function requestQuery(response: Response) {
   if (!response.ok) {
     throw new HttpError(response.status, response.statusText, response.url);
   }
