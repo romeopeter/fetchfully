826855c1abfee303f05a1a219baf4d73fa0ca1d8
romeo <romeomeshack34@gmail.com>
2025-06-18 17:42:01 +0100

    Attached response type

--- src/types/consumable-methods.ts
+++ src/types/consumable-methods.ts
@@ -1,7 +1,29 @@
+import { FetchfullyResponse } from "./fetchfully-response";
+
+/* ------------------------------------------------------------- */
+
 export interface RequestMethods {
-  get<T = any>(path: string, query?: Record<string, any>): Promise<T>;
-  post<T = any>(path: string, data?: any, query?: Record<string, any>): Promise<T>;
-  put<T = any>(path: string, data?: any, query?: Record<string, any>): Promise<T>;
-  patch<T = any>(path: string, data?: any, query?: Record<string, any>): Promise<T>;
-  delete<T = any>(path: string, query?: Record<string, any>): Promise<T>;
+  get<T = any>(
+    path: string,
+    query?: Record<string, any>
+  ): Promise<FetchfullyResponse<T>>;
+  post<T = any>(
+    path: string,
+    data?: any,
+    query?: Record<string, any>
+  ): Promise<FetchfullyResponse<T>>;
+  put<T = any>(
+    path: string,
+    data?: any,
+    query?: Record<string, any>
+  ): Promise<FetchfullyResponse<T>>;
+  patch<T = any>(
+    path: string,
+    data?: any,
+    query?: Record<string, any>
+  ): Promise<FetchfullyResponse<T>>;
+  delete<T = any>(
+    path: string,
+    query?: Record<string, any>
+  ): Promise<FetchfullyResponse<T>>;
 }
