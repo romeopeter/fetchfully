221904be6a22371c59252707a7b72f79f5c6be60
romeo <romeomeshack34@gmail.com>
2025-06-24 21:02:47 +0100

    Added refetch method and renamed response object.

--- src/request-query.ts
+++ src/request-query.ts
@@ -1,19 +1,52 @@
-import responseByContentType from "./utils/response-by-content-type";
+import filterByContentType from "./utils/filter-by-content-type";
 import { HttpError } from "./utils/custom-request-errors";
+import fetchfullyResponse from "./response";
+import { FetchfullyConfig } from "./types/config";
+import { FetchfullyResponse } from "./types/fetchfully-response";
 
 /* ------------------------------------------------------ */
+
+type Override = (
+  override?: Partial<FetchfullyConfig>
+) => Promise<FetchfullyResponse>;
 
 /**
  * Handles all non-mutation request (GET) cases.
  *
  * @param response Response: Fetch API response interface for a request.
- * 
- * @returns Promise<any>
+ * @param refetch Override: Re-execute user query
+ * @returns FetchfullyResponse<any>
  */
-export default function requestQuery(response: Response) {
-  if (!response.ok) {
-    throw new HttpError(response.status, response.statusText, response.url);
+export default function requestQuery(
+  originResponse: Response,
+  refetch?: Override
+) {
+  let response = fetchfullyResponse(
+    "loading",
+    null,
+    null,
+    undefined,
+    undefined
+  );
+
+  if (originResponse.ok) {
+    const data = filterByContentType(originResponse);
+
+    response = fetchfullyResponse(
+      "success",
+      data,
+      null,
+      originResponse.status,
+      originResponse.headers,
+      refetch
+    );
+  } else {
+    throw new HttpError(
+      response.status,
+      originResponse.statusText,
+      originResponse.url
+    );
   }
 
-  return responseByContentType(response);
+  return response;
 }
