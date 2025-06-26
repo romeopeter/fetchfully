74e52c9ca79f37d5135af7ed561617f54c5f58a1
romeo <romeomeshack34@gmail.com>
2025-06-26 14:54:37 +0100

    Default config

--- src/default-config.ts
+++ src/default-config.ts
@@ -12,7 +12,7 @@ export const defaultConfig: FetchfullyConfig = {
   queryArrayFormat: "comma",
   timeout: 0,
   headers: {
-    "Content-Type": "application/json; charset=UTF-8",
+    "Content-Type": "application/json",
     "Cache-Control": "no-cache",
   },
 };
