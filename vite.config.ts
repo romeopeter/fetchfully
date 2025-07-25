dfe8e3c3be814fd71f843c65756c697624643458
romeo <romeomeshack34@gmail.com>
2025-07-25 23:44:27 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: "./src/index.ts",
-      name: "simple-fetcher",
+      name: "fetchfully",
       formats: ["es", "umd"],
       fileName: (format) => `fetchfully.${format === "es" ? "mjs" : "cjs"}`,
     },
